8b5676bead492e9ca910d8ebfe185db98efbf203
mariuschev <marius.cheva@gmail.com>
2022-10-31 09:16:55 +0100

    saisie sécurisée afin de ne pas avoir à relancer le programme

--- main.c
+++ main.c
@@ -20,6 +20,10 @@ int main(){
     char dicos[][100] = {"..\\dictionnaire_non_accentue.txt","..\\dico_10_lignes.txt","..\\mots_courts.txt"};
     printf("Quel dictionnaire voulez vous utiliser ?\n\t1) %s\n\t2) %s\n\t3) %s\nVotre choix : ",dicos[0],dicos[1],dicos[2]);
     scanf("%d",&choice);
+    while(choice<1 || choice>3){
+        printf("Erreur...Quel dictionnaire voulez vous utiliser ?\n\t1) %s\n\t2) %s\n\t3) %s\nVotre choix : ",dicos[0],dicos[1],dicos[2]);
+        scanf("%d",&choice);
+    }
     loadTrees(dicos[choice-1], t_nom, t_ver, t_adj, t_adv);
 
     printf("%s %s %s %s\n",findRandomWord(t_nom),findRandomWord(t_adj),findRandomWord(t_ver),findRandomWord(t_nom));
